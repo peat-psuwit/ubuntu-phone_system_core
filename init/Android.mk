617470a426bba96a343000f320864a90d6aab758
รัชนันท์ ศรีรัตนเมธ <peathot@hotmail.com>
2015-03-13 02:51:25 +0700

    Adding vendor init.
    This is copied from Cyanogenmod in order to support unified tree of
    multi-sim device that use some properties to change rild's bahavior.

--- init/Android.mk
+++ init/Android.mk
@@ -16,7 +16,8 @@ LOCAL_SRC_FILES:= \
 	init_parser.c \
 	ueventd.c \
 	ueventd_parser.c \
-	watchdogd.c
+	watchdogd.c \
+	vendor_init.c
 
 ifeq ($(strip $(INIT_BOOTCHART)),true)
 LOCAL_SRC_FILES += bootchart.c
@@ -42,6 +43,15 @@ LOCAL_STATIC_LIBRARIES := \
 	libselinux \
 	libmincrypt \
 	libext4_utils_static
+
+#From Cyanogenmod: This allow me to use unified tree (too lazy to modify
+#my own tree!)
+ifneq ($(strip $(TARGET_PLATFORM_DEVICE_BASE)),)
+LOCAL_CFLAGS += -D_PLATFORM_BASE="\"$(TARGET_PLATFORM_DEVICE_BASE)\""
+endif
+ifneq ($(strip $(TARGET_INIT_VENDOR_LIB)),)
+LOCAL_WHOLE_STATIC_LIBRARIES += $(TARGET_INIT_VENDOR_LIB)
+endif
 
 include $(BUILD_EXECUTABLE)
 
